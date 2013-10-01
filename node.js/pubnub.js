5b82f3494c5f841ffc030f920c86ccce83c09fef
Gustav Rydstedt <gustav.rydstedt@gmail.com>
2013-10-01 15:20:21 -0700

    Catch errors from http(s) request, will throw globally otherwise.
    An error from the sending a request (such as a ECONNRESET), will throw a global exception.

--- node.js/pubnub.js
+++ node.js/pubnub.js
@@ -1063,6 +1063,7 @@ function xdr( setup ) {
                 finished();
             });
         });
+        request.on( 'error', function(){done(1, { "error" : "Network Connection Error"})});
         request.end();
         request.timeout = xhrtme;
 
