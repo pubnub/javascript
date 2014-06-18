2c273ad046c0f3db75ff152180b965e6f6356c0d
Geremy Cohen <gcohen@pubnub.com>
2014-06-18 15:36:34 -0700

    Update pn_message.js

--- node.js/examples/pn_message.js
+++ node.js/examples/pn_message.js
@@ -57,7 +57,7 @@ c.callback = console.log
 c.error = console.log
 c.channel = 'my_channel'
 
-c.gcm = ['i am gcm array']
+c.gcm = {"message":"be sure not to send objects!", "foo":"bar" }
 c.apns.alert = "this is alert"
 c.apns.badge = 2
 c.apns.key = "hi am apns"
