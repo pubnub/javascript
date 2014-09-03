1dd48f3146f32437dd90589f893f9ff83fbeaaa8
gcohen <geremy@pubnub.com>
2014-09-03 00:19:00 -0700

    unsub, sub, pub working

--- web/examples/channelGroups/main.js
+++ web/examples/channelGroups/main.js
@@ -3,61 +3,142 @@ var SUB_KEY = $("#sub_key").val();
 var SECRET_KEY = $("#secret_key").val();
 var UUID = $("#uuid").val();
 
-var namespaceEnable = $("#namespaceEnable").prop('checked');
-var namespace = namespaceEnable && $("#namespace").val().length > 0 ? $("#namespace").val() : false;
+function namespaceInit() {
+    namespaceEnable = $("#namespaceEnable").prop('checked');
+    namespace = namespaceEnable && $("#namespace").val().length > 0 ? $("#namespace").val() : false;
+}
 
-var channelGroupEnable = $("#channelGroupEnable").prop('checked');
-var channelGroup = channelGroupEnable && $("#channelGroup").val().length > 0 ? $("#channelGroup").val() : false;
+namespaceInit();
+$("#namespace").bind("change", function () {
+    namespaceInit()
+});
 
-var channelEnable = $("#channelEnable").prop('checked');
-var channel = channelEnable && $("#channel").val().length > 0 ? $("#channel").val() : false;
+function channelGroupInit() {
+    channelGroupEnable = $("#channelGroupEnable").prop('checked');
+    channelGroup = channelGroupEnable && $("#channelGroup").val().length > 0 ? $("#channelGroup").val() : false;
+}
 
-var authEnable = $("#authEnable").prop('checked');
-var auth = authEnable && $("#auth").val().length > 0 ? $("#auth").val() : false;
+channelGroupInit();
+$("#channelGroup").bind("change", function () {
+    channelGroupInit()
+});
 
-var message = $("#message").val().length > 0 ? $("#message").val() : null;
+function channelInit() {
+    channelEnable = $("#channelEnable").prop('checked');
+    channel = channelEnable && $("#channel").val().length > 0 ? $("#channel").val() : false;
+}
 
-    pubnub = PUBNUB.init({
-        "subscribe_key": SUB_KEY,
-        "publish_key": PUB_KEY,
-        "secrect_key": SECRET_KEY,
-        "uuid": UUID
-    });
+channelInit();
+$("#channel").bind("change", function () {
+    channelInit()
+});
 
-pubnub.auth(auth);
+function authInit() {
+    authEnable = $("#authEnable").prop('checked');
+    auth = authEnable && $("#auth").val().length > 0 ? $("#auth").val() : false;
+}
+
+authInit();
+$("#auth").bind("change", function () {
+    authInit()
+});
+
+function messageInit() {
+    message = $("#message").val().length > 0 ? $("#message").val() : null;
+}
+
+messageInit();
+$("#message").bind("change", function () {
+    messageInit();
+});
+
+pubnub = PUBNUB.init({
+    "subscribe_key": SUB_KEY,
+    "publish_key": PUB_KEY,
+    "secrect_key": SECRET_KEY,
+    "uuid": UUID
+});
 
 
-
-function pnTime(){
+function pnTime() {
     pubnub.time(
-        function(time){
+        function (time) {
             console.log(time);
             $("#output").html(time);
         }
     );
 }
 
-function pnPublish(){
+function pnPublish() {
     if (channel) {
         pubnub.publish({
             channel: channel,
             message: $("#message").val(),
-            callback: function(r){
-                console.log(JSON.stringify(r));
-                $("#output").html(JSON.stringify(r));
-            },
-            error: function(r){
-                console.log(JSON.stringify(r));
-                $("#output").html(JSON.stringify(r));
-            }
+            callback: displayCallback,
+            error: displayCallback
         });
     }
 }
 
-$("#publish").click(function(){
-   pnPublish();
+function displayCallback(m, e, c) {
+    if (c) {
+        console.log(JSON.stringify(c + ": " + m));
+        $("#output").html(c + ":" + JSON.stringify(m) + "\n" + $("#output").html());
+
+
+    } else {
+        console.log(JSON.stringify(m));
+        $("#output").html(JSON.stringify(m) + "\n" + $("#output").html());
+
+    }
+}
+
+function pnSubscribe() {
+    if (channel) {
+        pubnub.subscribe({
+            channel: channel,
+            callback: displayCallback,
+            error: displayCallback
+        });
+    } else if (channelGroup) {
+        pubnub.subscribe({
+            registry: channelGroup,
+            namespace: namespace,
+            callback: displayCallback,
+            error: displayCallback
+        });
+    }
+}
+
+function pnUnsubscribe() {
+    if (channel) {
+        pubnub.unsubscribe({
+            channel: channel,
+            callback: displayCallback,
+            error: displayCallback
+        });
+    } else if (channelGroup) {
+        pubnub.unsubscribe({
+            registry: channelGroup,
+            namespace: namespace,
+            callback: displayCallback,
+            error: displayCallback
+        });
+    }
+}
+
+$("#publish").click(function () {
+    pnPublish();
 });
 
-$("#time").click(function(){
+$("#time").click(function () {
     pnTime();
+});
+
+$("#subscribe").click(function () {
+    pnSubscribe();
+});
+
+$("#unsubscribe").click(function () {
+    pnUnsubscribe();
 });
