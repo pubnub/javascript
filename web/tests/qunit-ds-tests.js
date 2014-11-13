1109ceed18c472026205bbbf7f4b7be762c68a69
Devendra <devendra@udgama.com>
2014-11-14 03:10:01 +0530

    changeing origin in tests

--- web/tests/qunit-ds-tests.js
+++ web/tests/qunit-ds-tests.js
@@ -1,7 +1,7 @@
 var pubnub = PUBNUB({
     write_key     : "pub-c-bf446f9e-dd7f-43fe-8736-d6e5dce3fe67",
     read_key      : "sub-c-d1c2cc5a-1102-11e4-8880-02ee2ddab7fe",
-    origin        : "dara25.devbuild.pubnub.com"
+    origin        : "pubsub.pubnub.com"
 
 });
 
