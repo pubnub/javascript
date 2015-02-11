dc4856e719d23a9dde05c7652f7531597a84290a
Alexander Novikov <anovikov@granpm.com>
2015-02-11 10:23:20 +0100

    enable keepAliveAgent

--- node.js/pubnub.js
+++ node.js/pubnub.js
@@ -2085,7 +2085,7 @@ function xdr( setup ) {
     options.headers  = proxy ? { 'Host': origin }:null;
     options.method   = mode;
     options.keepAlive= !!keepAliveAgent;
-    //options.agent    = keepAliveAgent;    
+    options.agent    = keepAliveAgent;
     options.body     = payload;
 
     require('http').globalAgent.maxSockets = Infinity;
