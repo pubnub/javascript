3fc9bc6a38b074d9561ac9ad70825d969333411e
Devendra <devendra@udgama.com>
2015-03-13 14:07:58 +0530

    bumping version to 3.7.9

--- node.js/pubnub.js
+++ node.js/pubnub.js
@@ -1,4 +1,4 @@
-// Version: 3.7.8
+// Version: 3.7.9
 var NOW             = 1
 ,   READY           = false
 ,   READY_BUFFER    = []
@@ -12,7 +12,7 @@ var NOW             = 1
 ,   PARAMSBIT       = '&'
 ,   PRESENCE_HB_THRESHOLD = 5
 ,   PRESENCE_HB_DEFAULT  = 30
-,   SDK_VER         = '3.7.8'
+,   SDK_VER         = '3.7.9'
 ,   REPL            = /{([\w\-]+)}/g;
 
 /**
@@ -2002,7 +2002,7 @@ var NOW                 = 1
 ,   XHRTME              = 310000
 ,   DEF_TIMEOUT         = 10000
 ,   SECOND              = 1000
-,   PNSDK               = 'PubNub-JS-' + 'Nodejs' + '/' +  '3.7.8'
+,   PNSDK               = 'PubNub-JS-' + 'Nodejs' + '/' +  '3.7.9'
 ,   crypto              = require('crypto')
 ,   proxy               = null
 ,   XORIGN              = 1;
