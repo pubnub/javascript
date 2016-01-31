6329f45b0c0ac72b7ea5875ead51862a819f7fab
Max Presman <max@pubnub.com>
2016-01-31 02:16:14 -0800

    rework node.js networking stack, introduce full closure and cleanup node.js entry point

--- node.js/pubnub.js
+++ node.js/pubnub.js
@@ -31,41 +31,14 @@
  --------------------------------------------------------------------------- */
 
 var pubNubCore = require('../core/pubnub-common.js');
+var XDR = require('./lib/xdr');
 var packageJSON = require('../package.json');
 var crypto = require('crypto');
-var http = require('http');
-var https = require('https');
 
 /**
  * UTIL LOCALS
  */
 var PNSDK = 'PubNub-JS-' + 'Nodejs' + '/' + packageJSON.version;
-var proxy = null;
-var keepAliveConfig = {
-  keepAlive: true,
-  keepAliveMsecs: 300000,
-  maxSockets: 5
-};
-var keepAliveAgent;
-var keepAliveAgentSSL;
-
-function keepAliveIsEmbedded() {
-  return 'EventEmitter' in http.Agent.super_;
-}
-
-
-if (keepAliveIsEmbedded()) {
-  keepAliveAgent = new http.Agent(keepAliveConfig);
-  keepAliveAgentSSL = new https.Agent(keepAliveConfig);
-} else {
-  (function () {
-    var agent = require('agentkeepalive');
-    var agentSSL = agent.HttpsAgent;
-
-    keepAliveAgent = new agent(keepAliveConfig);
-    keepAliveAgentSSL = new agentSSL(keepAliveConfig);
-  })();
-}
 
 function getHMACSHA256(data, key) {
   return crypto.createHmac('sha256', new Buffer(key, 'utf8')).update(data).digest('base64');
@@ -78,155 +51,9 @@ function getHMACSHA256(data, key) {
  * error('message');
  */
 function error(message) {
-  console.error(message);
+  console.error(message); // eslint-disable-line no-console
 }
 
-/**
- * Request
- * =======
- *  xdr({
- *     url     : ['http://www.blah.com/url'],
- *     success : function(response) {},
- *     fail    : function() {}
- *  });
- */
-function xdr(setup) {
-  var request;
-  var response;
-  var debug = setup.debug;
-  var success = setup.success || function () {};
-  var fail = setup.fail || function () {};
-  var ssl = setup.ssl;
-  var complete = 0;
-  var loaded = 0;
-  var mode = setup.mode || 'GET';
-  var data = setup.data || {};
-  var xhrtme = setup.timeout || pubNubCore.DEF_TIMEOUT;
-  var body = '';
-
-  var timer;
-
-  var finished = function () {
-    if (loaded) return;
-    loaded = 1;
-
-    clearTimeout(timer);
-    try {
-      response = JSON.parse(body);
-    } catch (r) {
-      return done(1, { error: true, message: 'error in response parsing' });
-    }
-    success(response);
-  };
-
-  var done = function (failed, response) {
-    if (complete) return;
-    complete = 1;
-
-    clearTimeout(timer);
-
-    if (request) {
-      request.on('error', function () {});
-      request.on('data', function () {});
-      request.on('end', function () {});
-
-      if (request.abort) {
-        request.abort();
-      }
-
-      request = null;
-    }
-
-    if (failed) {
-      fail(response);
-    }
-  };
-
-  timer = pubNubCore.timeout(function () {
-    done(1, { error: true, message: 'timeout' });
-  }, xhrtme);
-
-  data.pnsdk = PNSDK;
-
-  var options = {};
-  var payload = '';
-
-  if (mode === 'POST') {
-    payload = decodeURIComponent(setup.url.pop());
-  }
-
-  var url = pubNubCore.build_url(setup.url, data);
-
-  if (debug) {
-    debug(url);
-  }
-
-  if (!ssl) ssl = (url.split('://')[0] === 'https');
-
-  url = '/' + url.split('/').slice(3).join('/');
-
-  var origin = setup.url[0].split('//')[1];
-
-  options.hostname = proxy ? proxy.hostname : setup.url[0].split('//')[1];
-  options.port = proxy ? proxy.port : ssl ? 443 : 80;
-  options.path = proxy ? 'http://' + origin + url : url;
-  options.headers = proxy ? { Host: origin } : null;
-  options.method = mode;
-  options.keepAlive = !!keepAliveAgent;
-  options.body = payload;
-
-  if (options.keepAlive && ssl) {
-    options.agent = keepAliveAgentSSL;
-  } else if (options.keepAlive) {
-    options.agent = keepAliveAgent;
-  }
-
-  require('http').globalAgent.maxSockets = Infinity;
-
-  try {
-    request = (ssl ? https : http).request(options, function (response) {
-      response.setEncoding('utf8');
-      response.on('error', function () {
-        done(1, body || { error: true, message: 'Network Connection Error' });
-      });
-      response.on('abort', function () {
-        done(1, body || { error: true, message: 'Network Connection Error' });
-      });
-      response.on('data', function (chunk) {
-        if (chunk) body += chunk;
-      });
-      response.on('end', function () {
-        var statusCode = response.statusCode;
-
-        switch (statusCode) {
-          case 200:
-            break;
-          default:
-            try {
-              response = JSON.parse(body);
-              done(1, response);
-            } catch (r) {
-              return done(1, { status: statusCode, payload: null, message: body });
-            }
-            return;
-        }
-        finished();
-      });
-    });
-    request.timeout = xhrtme;
-    request.on('error', function () {
-      done(1, { error: true, message: 'Network Connection Error' });
-    });
-
-    if (mode === 'POST') request.write(payload);
-    request.end();
-  } catch (e) {
-    done(0);
-    return xdr(setup);
-  }
-
-  return done;
-}
 
 /**
  * LOCAL STORAGE
@@ -245,25 +72,15 @@ var db = (function () {
 
 
 var CREATE_PUBNUB = function (setup) {
-  proxy = setup.proxy;
-  setup.xdr = xdr;
+  setup.xdr = XDR.createInstance(PNSDK, setup.proxy, setup.keepAlive);
   setup.db = db;
   setup.error = setup.error || error;
   setup.hmac_SHA256 = getHMACSHA256;
-  setup.crypto_obj = require('./lib/cryptoUtil');
+  setup.crypto_obj = require('./lib/crypto-wrapper');
   setup.params = { pnsdk: PNSDK };
   setup.shutdown = function () {
-    if (keepAliveAgentSSL && keepAliveAgentSSL.destroy) {
-      keepAliveAgentSSL.destroy();
-    }
-    if (keepAliveAgent && keepAliveAgent.destroy) {
-      keepAliveAgent.destroy();
-    }
+    XDR.destroyKeepAlive();
   };
-
-  if (setup.keepAlive === false) {
-    keepAliveAgent = undefined;
-  }
 
   var SELF = function (setup) {
     return CREATE_PUBNUB(setup);
@@ -284,7 +101,7 @@ var CREATE_PUBNUB = function (setup) {
 
   SELF.init = SELF;
   SELF.secure = SELF;
-  SELF.crypto_obj = require('./lib/cryptoUtil');
+  SELF.crypto_obj = require('./lib/crypto-wrapper');
 
   // TODO: remove dependence
   SELF.__PN = PN;
@@ -299,6 +116,7 @@ var CREATE_PUBNUB = function (setup) {
 CREATE_PUBNUB.init = CREATE_PUBNUB;
 CREATE_PUBNUB.unique = pubNubCore.unique;
 CREATE_PUBNUB.secure = CREATE_PUBNUB;
-CREATE_PUBNUB.crypto_obj = require('./lib/cryptoUtil');
+CREATE_PUBNUB.crypto_obj = require('./lib/crypto-wrapper');
+
 module.exports = CREATE_PUBNUB;
 module.exports.PNmessage = pubNubCore.PNmessage;
