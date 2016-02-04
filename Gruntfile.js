e244712ec1e6875c51b437388bea7ee131e52aa9
Max Presman <max@pubnub.com>
2016-02-04 15:57:43 -0800

    adjust grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
         src: 'node.js/tests/integration/stubbed',
         options: {
           coverageFolder: 'coverage/integration',
-          mask: '**/*.test.js',
+          mask: '*.test.js',
           print: 'none'
         }
       },
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
         options: {
           mochaOptions: ['--bail'],
           coverageFolder: 'coverage/unit',
-          mask: '**/*.test.js',
+          mask: '*.test.js',
           print: 'none'
         }
       },
