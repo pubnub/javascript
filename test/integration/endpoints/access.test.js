78a8a528c0d49d968cb07077e2dd7c494d9f600d
Max Presman <max@pubnub.com>
2016-07-22 19:08:13 -0700

    update signatures

--- test/integration/endpoints/access.test.js
+++ test/integration/endpoints/access.test.js
@@ -13,7 +13,7 @@ describe('access endpoints', () => {
 
   before(() => {
     nock.disableNetConnect();
-    clock = sinon.useFakeTimers(new Date(2011, 9, 1).getTime());
+    clock = sinon.useFakeTimers(new Date(Date.UTC(2011, 9, 1, 0, 0, 0)).getTime());
   });
 
   after(() => {
@@ -23,14 +23,14 @@ describe('access endpoints', () => {
 
   beforeEach(() => {
     nock.cleanAll();
-    pubnub = new PubNub({ subscribeKey: 'mySubscribeKey', publishKey: 'myPublishKey', secretKey: 'mySecretKey', uuid: 'myUUID', logVerbosity: true });
+    pubnub = new PubNub({ subscribeKey: 'mySubscribeKey', publishKey: 'myPublishKey', secretKey: 'mySecretKey', uuid: 'myUUID' });
     pubnub._config.baseParams.pnsdk = 'PubNub-JS-Nodejs/suchJavascript';
   });
 
   describe('#audit', () => {
     it('issues the correct RESTful request for channels', (done) => {
       const scope = utils.createNock().get('/v1/auth/audit/sub-key/mySubscribeKey')
-      .query({ timestamp: 1317452400, channel: 'ch1', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: '9H37FYGAsuW3dgW2FISqcVj9bMPy+obPnSr/RzzMLWI=' })
+      .query({ timestamp: 1317427200, channel: 'ch1', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: '4EgDD2LM2BO9GqEwlHDeSBbCM0muLh+Y1s1FY3FrRQI=' })
       .reply(200, '{"message":"Success","payload":{"level":"channel-group+auth","subscribe_key":"sub-c-82ab2196-b64f-11e5-8622-0619f8945a4f","channel-group":"cg2","auths":{"key1":{"r":1,"m":1,"w":1}}},"service":"Access Manager","status":200}');
 
       pubnub.audit({ channel: 'ch1' }, (status, response) => {
@@ -54,7 +54,7 @@ describe('access endpoints', () => {
 
     it('issues the correct RESTful request for channel groups', (done) => {
       const scope = utils.createNock().get('/v1/auth/audit/sub-key/mySubscribeKey')
-      .query({ timestamp: 1317452400, 'channel-group': 'cg1', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: 'Ci+q0jVNlR4F0n2fDdU62q+DNIMRJ8FYIEoF0PyAOT0=' })
+      .query({ timestamp: 1317427200, 'channel-group': 'cg1', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: 'J9kaoNajCy6eMCGmL4Fs6LjePhhgkwL6UNW5lry2c4A=' })
       .reply(200, '{"message":"Success","payload":{"level":"channel-group+auth","subscribe_key":"sub-c-82ab2196-b64f-11e5-8622-0619f8945a4f","channel-group":"cg2","auths":{"key1":{"r":1,"m":1,"w":1}}},"service":"Access Manager","status":200}');
 
       pubnub.audit({ channelGroup: 'cg1' }, (status, response) => {
@@ -78,7 +78,7 @@ describe('access endpoints', () => {
 
     it('issues the correct RESTful request for keys', (done) => {
       const scope = utils.createNock().get('/v1/auth/audit/sub-key/mySubscribeKey')
-      .query({ timestamp: 1317452400, auth: 'key1,key2', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: 'FcpPp2j9rshekMnkVMSeDZrybm2F19hKoQHBhj4RTBE=' })
+      .query({ timestamp: 1317427200, auth: 'key1,key2', uuid: 'myUUID', pnsdk: 'PubNub-JS-Nodejs/suchJavascript', signature: 'jax2fEP+dRB6kV+pYQGIOEYQJZ3rugoZuYDH+rxPyk4=' })
       .reply(200, '{"message":"Success","payload":{"level":"channel-group+auth","subscribe_key":"sub-c-82ab2196-b64f-11e5-8622-0619f8945a4f","channel-group":"cg2","auths":{"key1":{"r":1,"m":1,"w":1}}},"service":"Access Manager","status":200}');
 
       pubnub.audit({ authKeys: ['key1', 'key2'] }, (status, response) => {
@@ -105,12 +105,12 @@ describe('access endpoints', () => {
     it('issues the correct RESTful request for channels', (done) => {
       const scope = utils.createNock().get('/v1/auth/grant/sub-key/mySubscribeKey')
       .query({
-        timestamp: 1317452400,
+        timestamp: 1317427200,
         channel: 'ch1,ch2',
         auth: 'key1,key2',
         uuid: 'myUUID',
         pnsdk: 'PubNub-JS-Nodejs/suchJavascript',
-        signature: '2tLHfhekVOliPm5C5sgQgMG7Lc3Tm3lVN2xFh8KpQU4=',
+        signature: 'GorjdSX4ct8sUbAAbMqq0ighKvVud8L5CpD2Z+RDnSQ=',
         r: 0, w: 0, m: 0
       })
       .reply(200, '{"message":"Success","payload":{"level":"channel-group+auth","subscribe_key":"sub-c-82ab2196-b64f-11e5-8622-0619f8945a4f","channel-group":"cg2","auths":{"key1":{"r":1,"m":1,"w":1}}},"service":"Access Manager","status":200}');
@@ -125,12 +125,12 @@ describe('access endpoints', () => {
     it('issues the correct RESTful request for channels groups', (done) => {
       const scope = utils.createNock().get('/v1/auth/grant/sub-key/mySubscribeKey')
       .query({
-        timestamp: 1317452400,
+        timestamp: 1317427200,
         'channel-group': 'cg1,cg2',
         auth: 'key1,key2',
         uuid: 'myUUID',
         pnsdk: 'PubNub-JS-Nodejs/suchJavascript',
-        signature: 'vwHrjpyZN19DbARCWS5CCg4neybN5y5Oo58+zJGB4EU=',
+        signature: 'MiYApTnEe6n67rqlMgFiU4csLnu+XJWIB8aAHWuKLhY=',
         r: 1, w: 1, m: 0
       })
       .reply(200, '{"message":"Success","payload":{"level":"channel-group+auth","subscribe_key":"sub-c-82ab2196-b64f-11e5-8622-0619f8945a4f","channel-group":"cg2","auths":{"key1":{"r":1,"m":1,"w":1}}},"service":"Access Manager","status":200}');
@@ -145,12 +145,12 @@ describe('access endpoints', () => {
     it('issues the correct RESTful request for channels groups w/ ttl', (done) => {
       const scope = utils.createNock().get('/v1/auth/grant/sub-key/mySubscribeKey')
       .query({
-        timestamp: 1317452400,
+        timestamp: 1317427200,
         'channel-group': 'cg1,cg2',
         auth: 'key1,key2',
         uuid: 'myUUID',
         pnsdk: 'PubNub-JS-Nodejs/suchJavascript',
-        signature: 'V1a6caCWbFO+CfkUUov9mYB9VAf4xseSDUAtpAoLg7I=',
+        signature: 'i4U3Cg3Sa5pCXAYOCGcqndwk/DZ1qyQc6J6xqT42uEc=',
         r: 1, w: 1, m: 0,
         ttl: 1337
       })
