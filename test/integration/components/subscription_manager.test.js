1d8f84d546627da559116dd16db620e467c12a77
Max Presman <max@pubnub.com>
2016-08-09 07:56:14 -0700

    adjust tests for 0.10

--- test/integration/components/subscription_manager.test.js
+++ test/integration/components/subscription_manager.test.js
@@ -22,12 +22,71 @@ describe('#components/subscription_manger', () => {
   beforeEach(() => {
     nock.cleanAll();
     pubnub = new PubNub({ subscribeKey: 'mySubKey', publishKey: 'myPublishKey', uuid: 'myUUID' });
-    pubnubWithPassingHeartbeats = new PubNub({ subscribeKey: 'mySubKey', publishKey: 'myPublishKey', uuid: 'myUUID', announceSuccessfulHeartbeats: true, logVerbosity: true });
+    pubnubWithPassingHeartbeats = new PubNub({ subscribeKey: 'mySubKey', publishKey: 'myPublishKey', uuid: 'myUUID', announceSuccessfulHeartbeats: true });
   });
 
   afterEach(() => {
     pubnub.stop();
     pubnubWithPassingHeartbeats.stop();
+  });
+
+  it('passes the correct message information', (done) => {
+    const scope1 = utils.createNock().get('/v2/subscribe/mySubKey/ch1%2Cch2%2Cch1-pnpres%2Cch2-pnpres/0')
+      .query({ pnsdk: 'PubNub-JS-Nodejs/' + pubnub.getVersion(), uuid: 'myUUID', heartbeat: 300 })
+      .reply(200, '{"t":{"t":"3","r":1},"m":[{"a":"4","f":0,"i":"Client-g5d4g","p":{"t":"14607577960925503","r":1},"k":"sub-c-4cec9f8e-01fa-11e6-8180-0619f8945a4f","c":"coolChannel","d":{"text":"Message"},"b":"coolChan-bnel"}]}');
+
+    const scope2 = utils.createNock().get('/v2/subscribe/mySubKey/ch1%2Cch2%2Cch1-pnpres%2Cch2-pnpres/0')
+      .query({ pnsdk: 'PubNub-JS-Nodejs/' + pubnub.getVersion(), uuid: 'myUUID', heartbeat: 300, tt: 3, tr: 1 })
+      .reply(200, '{"t":{"t":"10","r":1},"m":[{"a":"4","f":0,"i":"Client-g5d4g","p":{"t":"14607577960925503","r":1},"k":"sub-c-4cec9f8e-01fa-11e6-8180-0619f8945a4f","c":"coolChannel","d":{"text":"Message3"},"b":"coolChan-bnel"}]}');
+
+    const scope3 = utils.createNock().get('/v2/subscribe/mySubKey/ch1%2Cch2%2Cch1-pnpres%2Cch2-pnpres/0')
+      .query({ pnsdk: 'PubNub-JS-Nodejs/' + pubnub.getVersion(), uuid: 'myUUID', heartbeat: 300, tt: 10, tr: 1 })
+      .reply(200, '{"t":{"t":"20","r":1},"m":[{"a":"4","f":0,"i":"Client-g5d4g","p":{"t":"14607577960925503","r":1},"k":"sub-c-4cec9f8e-01fa-11e6-8180-0619f8945a4f","c":"coolChannel","d":{"text":"Message10"},"b":"coolChan-bnel"}]}');
+
+    let incomingPayloads = [];
+
+    pubnub.addListener({
+      message(messagePayload) {
+        incomingPayloads.push(messagePayload);
+
+        if (incomingPayloads.length === 3) {
+          assert.equal(scope1.isDone(), true);
+          assert.equal(scope2.isDone(), true);
+          assert.equal(scope3.isDone(), true);
+          assert.deepEqual(incomingPayloads, [
+            {
+              actualChannel: 'coolChannel',
+              message: {
+                text: 'Message'
+              },
+              subscribedChannel: 'coolChan-bnel',
+              timetoken: '14607577960925503',
+            },
+            {
+              actualChannel: 'coolChannel',
+              message: {
+                text: 'Message3',
+              },
+              subscribedChannel: 'coolChan-bnel',
+              timetoken: '14607577960925503',
+            },
+            {
+              actualChannel: 'coolChannel',
+              message: {
+                text: 'Message10',
+              },
+              subscribedChannel: 'coolChan-bnel',
+              timetoken: '14607577960925503',
+            }
+          ]);
+          done();
+        }
+
+        console.log(messagePayload);
+      }
+    });
+
+    pubnub.subscribe({ channels: ['ch1', 'ch2'], withPresence: true });
   });
 
   it('passes the correct presence information', (done) => {
@@ -46,7 +105,7 @@ describe('#components/subscription_manger', () => {
           timetoken: '14614512228418349',
           uuid: '4a6d5df7-e301-4e73-a7b7-6af9ab484eb0',
           action: 'join',
-          state: null
+          state: undefined
         }, presencePayload);
         done();
       }
