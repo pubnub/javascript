15a0868ee7b8017f5b9d0399a82a0b00064923f3
Max Presman <max@pubnub.com>
2016-11-01 19:25:16 -0700

    4.2.4

--- dist/web/pubnub.js
+++ dist/web/pubnub.js
@@ -1,4 +1,4 @@
-/*! 4.2.3 / Consumer  */
+/*! 4.2.4 / Consumer  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -2752,7 +2752,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = {
 		"name": "pubnub",
 		"preferGlobal": false,
-		"version": "4.2.3",
+		"version": "4.2.4",
 		"author": "PubNub <support@pubnub.com>",
 		"description": "Publish & Subscribe Real-time Messaging with PubNub",
 		"bin": {},
