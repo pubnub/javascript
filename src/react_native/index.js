57406b2cc9d0c8996e839c3fbd30bb285aeb93ee
Max Presman <max@presman.ca>
2017-02-06 23:20:03 -0800

    react native builds

---
+++ src/react_native/index.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+import PubNubCore from '../core/pubnub-common';
+import { InternalSetupStruct } from '../core/flow_interfaces';
+
+let Database = class {
+
+  storage: Object;
+
+  constructor() {
+    this.storage = {};
+  }
+
+  get(key) {
+    return this.storage[key];
+  }
+
+  set(key, value) {
+    this.storage[key] = value;
+  }
+};
+
+export default class extends PubNubCore {
+
+  constructor(setup: InternalSetupStruct) {
+    setup.db = new Database();
+    setup.sdkFamily = 'Nodejs';
+    super(setup);
+  }
+
+}
