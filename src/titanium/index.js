b8554ddd79c9277cafd50ef1df1b253f546c8cd3
manuelfernando <manuelcamaron@hotmail.com>
2017-03-23 16:25:05 -0500

    add titanium distribution

---
+++ src/titanium/index.js
@@ -0,0 +1,18 @@
+/* @flow */
+/* global localStorage, navigator, window */
+
+import PubNubCore from '../core/pubnub-common';
+import Networking from '../networking';
+import Database from '../db/common';
+import { get, post } from '../networking/modules/titanium';
+import { InternalSetupStruct } from '../core/flow_interfaces';
+
+export default class extends PubNubCore {
+  constructor(setup: InternalSetupStruct) {
+    setup.db = new Database();
+    setup.sdkFamily = 'TitaniumSDK';
+    setup.networking = new Networking({ get, post });
+
+    super(setup);
+  }
+}
