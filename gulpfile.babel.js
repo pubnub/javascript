194d399e22ca63aca2aa76e61496990bba03f5ba
Thomas Conner <thomas@kinvey.com>
2017-09-29 16:28:40 -0400

    Add tasks for to gulpfile for nativescript

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -51,6 +51,12 @@ gulp.task('compile_titanium', () => {
     .pipe(gulp.dest('dist/titanium'));
 });
 
+gulp.task('compile_nativescript', () => {
+  return gulp.src('src/nativescript/index.js')
+    .pipe(gulpWebpack(webpackConfig))
+    .pipe(gulp.dest('dist/nativescript'));
+});
+
 gulp.task('create_version', () => {
   return gulp.src('dist/web/pubnub.js')
     .pipe(rename(`pubnub.${packageJSON.version}.js`))
@@ -79,6 +85,13 @@ gulp.task('uglify_titanium', () => {
     .pipe(uglify({ mangle: true, compress: true }))
     .pipe(rename('pubnub.min.js'))
     .pipe(gulp.dest('dist/titanium'));
+});
+
+gulp.task('uglify_nativescript', () => {
+  return gulp.src('dist/nativescript/pubnub.js')
+    .pipe(uglify({ mangle: true, compress: true }))
+    .pipe(rename('pubnub.min.js'))
+    .pipe(gulp.dest('dist/nativescript'));
 });
 
 gulp.task('lint_code', [], () => {
@@ -155,9 +168,9 @@ gulp.task('test', () => {
 });
 
 gulp.task('webpack', (done) => {
-  runSequence('compile_web', 'compile_titanium', done);
+  runSequence('compile_web', 'compile_titanium', 'compile_nativescript', done);
 });
 
 gulp.task('compile', (done) => {
-  runSequence('clean', 'babel', 'webpack', 'uglify_web', 'uglify_titanium', 'create_version', 'create_version_gzip', done);
+  runSequence('clean', 'babel', 'webpack', 'uglify_web', 'uglify_titanium', 'uglify_nativescript', 'create_version', 'create_version_gzip', done);
 });
