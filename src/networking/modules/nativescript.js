c5267d036dca986f49a807df897bec3b46ec1d0e
Thomas Conner <thomas@kinvey.com>
2017-10-05 09:19:10 -0400

    Fix error detection for NartiveScript network module

--- src/networking/modules/nativescript.js
+++ src/networking/modules/nativescript.js
@@ -56,8 +56,8 @@ function xdr(method: string, url: string, params: Object, body: string, endpoint
     })
     .catch((e) => {
       status.error = true;
-      status.errorData = e.error;
-      status.category = this._detectErrorCategory(e.error);
+      status.errorData = e;
+      status.category = this._detectErrorCategory(e);
       callback(status, null);
     });
 }
