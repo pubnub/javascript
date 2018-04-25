e903f30e6cc37330b2cb17e66e1e62b16ac2e31b
manuelfernando <manuelcamaron@hotmail.com>
2018-04-24 21:16:48 -0500

    v4.20.3

--- src/core/components/config.js
+++ src/core/components/config.js
@@ -228,7 +228,7 @@ export default class {
   setSendBeaconConfig(val: boolean): this { this._useSendBeacon = val; return this; }
 
   getVersion(): string {
-    return '4.20.2';
+    return '4.20.3';
   }
 
   _decideUUID(providedUUID: string): string {
