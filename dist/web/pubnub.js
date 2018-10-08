627b95671ca4b8cbb4959b0d53854237e369a3b5
Manuel Fernando <manuelcamaron@gmail.com>
2018-10-08 12:15:52 -0500

    Develop (#138)
    * fix POST for nativescript adapter over android
    
    * v4.21.6

--- dist/web/pubnub.js
+++ dist/web/pubnub.js
@@ -1,4 +1,4 @@
-/*! 4.21.5 / Consumer  */
+/*! 4.21.6 / Consumer  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -608,7 +608,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }, {
 	    key: 'getVersion',
 	    value: function getVersion() {
-	      return '4.21.5';
+	      return '4.21.6';
 	    }
 	  }, {
 	    key: '_decideUUID',
