bddc2b269a078d67d11fc8cac09afba8827b3c73
manuelfernando <manuelcamaron@hotmail.com>
2018-12-20 16:06:28 -0500

    4.21.7

--- dist/titanium/pubnub.js
+++ dist/titanium/pubnub.js
@@ -1,4 +1,4 @@
-/*! 4.21.6 / Consumer  */
+/*! 4.21.7 / Consumer  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -586,7 +586,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }, {
 	    key: 'getVersion',
 	    value: function getVersion() {
-	      return '4.21.6';
+	      return '4.21.7';
 	    }
 	  }, {
 	    key: '_decideUUID',
