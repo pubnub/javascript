8500cd1ab68b38a19a14a94d5835770d46a2d9ed
davidnub <david.lin@pubnub.com>
2019-07-03 13:08:44 -0600

    Update operation constant to match pattern
     - David

--- src/core/endpoints/users/update_user.js
+++ src/core/endpoints/users/update_user.js
@@ -20,7 +20,7 @@ function prepareMessagePayload(modules, messagePayload) {
 }
 
 export function getOperation(): string {
-  return operationConstants.PNUsersOperation;
+  return operationConstants.PNUpdateUserOperation;
 }
 
 export function validateParams(
