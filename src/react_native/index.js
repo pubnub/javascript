8a8cd9d3ac835672a1140846e07ea80ac1f1bcf4
Rai Phairow <rai.phairow@Rais-MacBook-Pro-2.local>
2022-06-21 08:38:13 -0700

    use base64 fix from clen-535

--- src/react_native/index.js
+++ src/react_native/index.js
@@ -2,18 +2,19 @@ import CborReader from 'cbor-js';
 import { Buffer } from 'buffer';
 import PubNubCore from '../core/pubnub-common';
 import Networking from '../networking';
+import { decode } from '../core/components/base64_codec';
 import Cbor from '../cbor/common';
 import { del, get, post, patch } from '../networking/modules/web-node';
 import { getfile, postfile } from '../networking/modules/react_native';
 
 import PubNubFile from '../file/modules/react-native';
-import { base64ToBinary, stringifyBufferKeys } from '../web';
+import { stringifyBufferKeys } from '../web';
 
 global.Buffer = global.Buffer || Buffer;
 
 export default class extends PubNubCore {
   constructor(setup) {
-    setup.cbor = new Cbor((arrayBuffer) => stringifyBufferKeys(CborReader.decode(arrayBuffer)), base64ToBinary);
+    setup.cbor = new Cbor((arrayBuffer) => stringifyBufferKeys(CborReader.decode(arrayBuffer)), decode);
 
     setup.PubNubFile = PubNubFile;
 
