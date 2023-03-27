376aceaf05569f485cd0310713285965636abe0c
Mohit Tejani <mohit.tejani@pubnub.com>
2023-03-27 11:03:54 +0530

    signal - test with type

--- test/contract/steps/publish/signalToSpace.ts
+++ test/contract/steps/publish/signalToSpace.ts
@@ -1,6 +1,6 @@
 import { When } from '@cucumber/cucumber';
 
-When('I send a signal with {string} space id and {string} message type', async function (spaceId, messageType) {
+When('I send a signal with {string} space id and {string} type', async function (spaceId, type) {
   const pubnub = this.getPubnub({
     publishKey: this.keyset.publishKey,
     subscribeKey: this.keyset.subscribeKey,
@@ -11,11 +11,11 @@ When('I send a signal with {string} space id and {string} message type', async f
       channel: 'test',
       message: { hello: 'world' },
       spaceId: spaceId,
-      messageType: messageType,
+      type: type,
     });
     this.isSucceed = typeof response.timetoken === 'string';
   } catch (status) {
-    if ((status as { error: boolean }).error) {
+    if ((status as { name: string; status: { error: boolean } }).status.error) {
       this.isSucceed = false;
     }
   }
