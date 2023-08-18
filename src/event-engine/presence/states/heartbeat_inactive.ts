90961004d446e2b0b0def56d5a6c145726873326
Mohit Tejani <mohit.tejani@pubnub.com>
2023-08-18 12:45:07 +0530

    fix: paths

--- src/event-engine/presence/states/heartbeat_inactive.ts
+++ src/event-engine/presence/states/heartbeat_inactive.ts
@@ -1,11 +1,11 @@
-import { State } from '../core/state';
+import { State } from '../../core/state';
 import { Effects } from '../effects';
 import { Events, joined } from '../events';
 import { HeartbeatingState } from './heartbeating';
 
 export const HeartbeatInactiveState = new State<void, Events, Effects>('HEARTBEAT_INACTIVE');
 
-HeartbeatInactiveState.on(joined.type, (_, event) => 
+HeartbeatInactiveState.on(joined.type, (context, event) => 
   HeartbeatingState.with({
     channels: event.payload.channels,
     groups: event.payload.groups
