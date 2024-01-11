f4c7f157b0e184a6691cfb00f373c1eb8db5b41e
Mohit Tejani <mohit.tejani@pubnub.com>
2024-01-11 14:24:19 +0530

    retryConfiguration behaviour updates

--- src/event-engine/core/retryPolicy.ts
+++ src/event-engine/core/retryPolicy.ts
@@ -1,16 +1,26 @@
 export class RetryPolicy {
+  static excludedErrorCodes = [403, 429];
   static LinearRetryPolicy(configuration: LinearRetryPolicyConfiguration) {
     return {
       delay: configuration.delay,
       maximumRetry: configuration.maximumRetry,
       shouldRetry(error: any, attempt: number) {
-        if (error?.status?.statusCode === 403) {
+        if (RetryPolicy.excludedErrorCodes.includes(error?.status?.statusCode)) {
           return false;
         }
         return this.maximumRetry > attempt;
       },
       getDelay(_: number) {
-        return this.delay * 1000;
+        return (this.delay + Math.random()) * 1000;
+      },
+      getGiveupReason(error: any, attempt: number) {
+        if (this.maximumRetry <= attempt) {
+          return 'retry attempts exhaused.';
+        }
+        if (RetryPolicy.excludedErrorCodes.includes(error?.status?.statusCode)) {
+          return 'forbidden or too many requests.';
+        }
+        return 'unknown error';
       },
     };
   }
@@ -29,12 +39,22 @@ export class RetryPolicy {
       },
 
       getDelay(attempt: number) {
-        const calculatedDelay = Math.trunc(Math.pow(2, attempt)) * 1000 + Math.random() * 1000;
-        if (calculatedDelay > 150000) {
-          return 150000;
+        const calculatedDelay = (Math.pow(2, attempt) + Math.random()) * 1000;
+        if (calculatedDelay > this.maximumDelay) {
+          return this.maximumDelay;
         } else {
           return calculatedDelay;
         }
+      },
+
+      getGiveupReason(error: any, attempt: number) {
+        if (this.maximumRetry <= attempt) {
+          return 'retry attempts exhaused.';
+        }
+        if (RetryPolicy.excludedErrorCodes.includes(error?.status?.statusCode)) {
+          return 'forbidden or too many requests.';
+        }
+        return 'unknown error';
       },
     };
   }
