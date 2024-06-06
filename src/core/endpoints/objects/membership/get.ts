5d55461b59c1aab7a408adca15ac5e11d1503a7c
Serhii Mamontov <sergey@pubnub.com>
2024-06-06 13:50:07 +0300

    fix(app-context): fix `sort` options serialization
    Fix issue because of which single string sort option wasn't serialized properly.

--- src/core/endpoints/objects/membership/get.ts
+++ src/core/endpoints/objects/membership/get.ts
@@ -133,9 +133,10 @@ export class GetUUIDMembershipsRequest<
 
   protected get queryParameters(): Query {
     const { include, page, filter, sort, limit } = this.parameters;
-    const sorting = Object.entries(sort ?? {}).map(([option, order]) =>
-      order !== null ? `${option}:${order}` : option,
-    );
+    let sorting: string | string[] = '';
+    if (typeof sort === 'string') sorting = sort;
+    else
+      sorting = Object.entries(sort ?? {}).map(([option, order]) => (order !== null ? `${option}:${order}` : option));
     const includeFlags: string[] = [];
 
     if (include!.statusField) includeFlags.push('status');
