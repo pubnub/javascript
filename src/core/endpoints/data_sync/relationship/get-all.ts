87340923705faf36bea34c6db251d321b20d6887
Mohit Tejani <mohit.tejani@Mohits-MacBook-Pro.local>
2026-05-22 13:31:01 +0530

    data-sync: entity-relationship endpoint apis

---
+++ src/core/endpoints/data_sync/relationship/get-all.ts
@@ -0,0 +1,75 @@
+/**
+ * Get All Relationships REST API module.
+ *
+ * @internal
+ */
+
+import { AbstractRequest } from '../../../components/request';
+import RequestOperation from '../../../constants/operations';
+import * as DataSync from '../../../types/api/data-sync';
+import { KeySet, Query } from '../../../types/api';
+
+// --------------------------------------------------------
+// ----------------------- Defaults -----------------------
+// --------------------------------------------------------
+// region Defaults
+
+/**
+ * Default number of items per page.
+ */
+const DEFAULT_LIMIT = 20;
+// endregion
+
+// --------------------------------------------------------
+// ------------------------ Types -------------------------
+// --------------------------------------------------------
+// region Types
+
+/**
+ * Request configuration parameters.
+ */
+type RequestParameters = DataSync.GetAllRelationshipsParameters & {
+  /**
+   * PubNub REST API access key set.
+   */
+  keySet: KeySet;
+};
+// endregion
+
+/**
+ * Get All Relationships request.
+ *
+ * @internal
+ */
+export class GetAllRelationshipsRequest<
+  Response extends DataSync.GetAllRelationshipsResponse,
+> extends AbstractRequest<Response, Response> {
+  constructor(private readonly parameters: RequestParameters) {
+    super();
+
+    // Apply defaults.
+    parameters.limit ??= DEFAULT_LIMIT;
+  }
+
+  operation(): RequestOperation {
+    return RequestOperation.PNGetAllRelationshipsOperation;
+  }
+
+  protected get path(): string {
+    return `/v1/datasync/subkeys/${this.parameters.keySet.subscribeKey}/relationships`;
+  }
+
+  protected get queryParameters(): Query {
+    const { entityAId, entityBId, cursor, limit, filter, sort, filterAdvanced } = this.parameters;
+
+    return {
+      ...(entityAId ? { entity_a_id: entityAId } : {}),
+      ...(entityBId ? { entity_b_id: entityBId } : {}),
+      ...(cursor ? { cursor } : {}),
+      ...(limit ? { limit: `${limit}` } : {}),
+      ...(filter ? { filter } : {}),
+      ...(sort ? { sort } : {}),
+      ...(filterAdvanced ? { filter_advanced: filterAdvanced } : {}),
+    };
+  }
+}
---
+++ src/core/endpoints/data_sync/relationship/get.ts
@@ -0,0 +1,57 @@
+/**
+ * Get Relationship REST API module.
+ *
+ * @internal
+ */
+
+import { AbstractRequest } from '../../../components/request';
+import RequestOperation from '../../../constants/operations';
+import * as DataSync from '../../../types/api/data-sync';
+import { KeySet } from '../../../types/api';
+import { encodeString } from '../../../utils';
+
+// --------------------------------------------------------
+// ------------------------ Types -------------------------
+// --------------------------------------------------------
+// region Types
+
+/**
+ * Request configuration parameters.
+ */
+type RequestParameters = DataSync.GetRelationshipParameters & {
+  /**
+   * PubNub REST API access key set.
+   */
+  keySet: KeySet;
+};
+// endregion
+
+/**
+ * Get Relationship request.
+ *
+ * @internal
+ */
+export class GetRelationshipRequest<
+  Response extends DataSync.GetRelationshipResponse,
+> extends AbstractRequest<Response, Response> {
+  constructor(private readonly parameters: RequestParameters) {
+    super();
+  }
+
+  operation(): RequestOperation {
+    return RequestOperation.PNGetRelationshipOperation;
+  }
+
+  validate(): string | undefined {
+    if (!this.parameters.id) return 'Relationship id cannot be empty';
+  }
+
+  protected get path(): string {
+    const {
+      keySet: { subscribeKey },
+      id,
+    } = this.parameters;
+
+    return `/v1/datasync/subkeys/${subscribeKey}/relationships/${encodeString(id)}`;
+  }
+}
