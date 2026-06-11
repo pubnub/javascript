94d70766d0a5deab45272bd49ba36566ad2b1f05
Mohit Tejani <60129002+mohitpubnub@users.noreply.github.com>
2026-06-11 14:31:51 +0530

    Fix/exponential retry policy (#499)
    * fix: exponential retry policy delay calculation should always respect minimum configured delay
    
    * dist/lib updates
    
    * PubNub SDK v11.0.2 release.
    
    ---------
    
    Co-authored-by: Mohit Tejani <mohit.tejani@Mohits-MacBook-Pro.local>
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- dist/web/pubnub.js
+++ dist/web/pubnub.js
@@ -5009,7 +5009,7 @@
 	                if (response && response.headers['retry-after'] !== undefined)
 	                    delay = parseInt(response.headers['retry-after'], 10);
 	                if (delay === -1)
-	                    delay = Math.min(Math.pow(2, attempt), this.maximumDelay);
+	                    delay = Math.min(this.minimumDelay * Math.pow(2, attempt), this.maximumDelay);
 	                return (delay + Math.random()) * 1000;
 	            },
 	            validate() {
@@ -5432,7 +5432,7 @@
 	            return base.PubNubFile;
 	        },
 	        get version() {
-	            return '11.0.1';
+	            return '11.0.2';
 	        },
 	        getVersion() {
 	            return this.version;
